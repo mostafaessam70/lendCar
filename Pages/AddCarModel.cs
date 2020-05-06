7b19bb36253192cdc79efc6e38e4e92baac5c6df
muhammad essam <muhammadessam295@gmail.com>
2020-05-07 00:18:26 +0200

    addcar pagemodel added

---
+++ Pages/AddCarModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LendCar.Pages
+{
+    public class AddCarModel:PageModel
+    {
+        
+    }
+}
