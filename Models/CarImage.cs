77b9f1e69aaac8a31b409b89ebb2934219425479
muhammad essam <muhammadessam295@gmail.com>
2020-05-10 15:15:13 +0200

    Model and addcar dates form modified

---
+++ Models/CarImage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LendCar.Models
+{
+    public class CarImage
+    {
+        public int Id { get; set; }
+      
+        [DataType(DataType.ImageUrl)]
+        public string Image { get; set; }
+        public int VehicleId { get; set; }
+        [ForeignKey("VehicleId")]
+        public Vehicle Vehicle { get; set; }
+    }
+}
---
+++ Models/Color.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LendCar.Models
+{
+    public class Color
+    {
+        public int Id { get; set; }
+        [Required,MaxLength(25)]
+        public string Name { get; set; }
+        [Required,MaxLength(7),RegularExpression("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$")]
+        public string HexValue { get; set; }
+        public ICollection<Vehicle> Vehicles { get; set; }
+    }
+}
