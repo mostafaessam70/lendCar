7ee353f614dfbf03a4f87f43ee34a2a252bf9551
Moneim <ahmedmoneim094@gmail.com>
2020-05-13 12:40:35 +0200

    Booking Edit

--- Areas/Cars/Pages/booking.cshtml.cs
+++ Areas/Cars/Pages/booking.cshtml.cs
@@ -7,6 +7,7 @@ using LendCar.Models;
 using LendCar.Repository;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -22,7 +23,6 @@ namespace LendCar.Pages
         public ICityRepostiory CityRepository { get; }
         public ICarRepository CarRepsitory { get; }
         public Contact Contact { get; set; }
-        public ApplicationUser LoggedUser { get; set; }
         [BindProperty]
         public ApplicationUser ModelUser { get; set; }
 
@@ -48,23 +48,22 @@ namespace LendCar.Pages
         public string StartBookingDate { get; set; }
 
         public Vehicle Vehicle { get; set; }
+        public int CarId { get; set; }
         public bookingModel(SignInManager<ApplicationUser> signInManager,
             UserManager<ApplicationUser> usermanger, IUserRepository userRerpository,
-            ICityRepostiory cityRepository, ICarRepository carRepsitory,IContactRepository IContactRepsotiory)
+            ICityRepostiory cityRepository, ICarRepository carRepsitory, IContactRepository IContactRepsotiory)
         {
             SignInManager = signInManager;
             Usermanger = usermanger;
             UserRerpository = userRerpository;
             CityRepository = cityRepository;
             CarRepsitory = carRepsitory;
-            LoggedUser = new ApplicationUser();
             Contact = IContactRepsotiory.GetCompanyContact();
 
         }
-        public async Task OnGetAsync(int carId)
+        public void OnGetAsync(int carId)
         {
-
-
+            CarId = carId;
             //await Usermanger.CreateAsync(new ApplicationUser()
             //{
             //    Email = "AhmedMoneim2@gmail.com",
@@ -75,16 +74,46 @@ namespace LendCar.Pages
             //    CityId = 1
             //}, "Sara@ask123.com");
             //await SignInManager.PasswordSignInAsync("Moneim2", "Sara@ask123.com", false, false);
-            this.Vehicle = CarRepsitory.GetVehicle(carId);
-            LoggedUser = await Usermanger.GetUserAsync(HttpContext.User);
+            this.Vehicle = CarRepsitory.GetVehicle(CarId);
 
+            string[] dateTime = UserRerpository.FindById(this.User.FindFirstValue(ClaimTypes.NameIdentifier))?.BirthDate?.Split('/');
+            if (dateTime?.Length == 3)
+            {
+                Day = dateTime[0];
+                Month = dateTime[1];
+                Year = dateTime[2];
+            }
 
         }
-        public void OnPost()
+        public void OnPost(int carId)
         {
-            ModelUser.Id = LoggedUser.Id;
-            UserRerpository.EditBookingInfo(ModelUser);
-            UserRerpository.Save();
+            this.Vehicle = CarRepsitory.GetVehicle(carId);
+            CarId = carId;
+
+            string[] dateTime = UserRerpository.FindById(this.User.FindFirstValue(ClaimTypes.NameIdentifier))?.BirthDate?.Split('/');
+            if (dateTime?.Length == 3)
+            {
+                Day = dateTime[0];
+                Month = dateTime[1];
+                Year = dateTime[2];
+            }
+
+
+
+            if (!ModelState.IsValid)
+            {
+                ModelUser.Id = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                ModelUser.BirthDate = (Day.Length == 2 ? Day : "0" + Day) + "/" +
+                    (Month.Length == 2 ? Month : "0" + Month) + "/" + Year;
+
+               
+                UserRerpository.EditBookingInfo(ModelUser);
+                UserRerpository.Save();
+
+            
+            }
+
         }
     }
 }
