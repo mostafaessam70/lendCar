3ab9e0b6617deaa338e9a5b8237d21e59df42967
muhammad essam <muhammadessam295@gmail.com>
2020-06-02 21:32:42 +0200

    links modified

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LendCar.Models;
+using LendCar.Repository;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -13,12 +14,16 @@ namespace LendCar.Pages
     public class HomeModel : PageModel
     {
         private SignInManager<ApplicationUser> _signInManager;
-        public HomeModel(SignInManager<ApplicationUser> signInManager)
+        private ICarRepository _carRepo;
+        public List<Vehicle> Vehicles { get; set; }
+        public HomeModel(SignInManager<ApplicationUser> signInManager, ICarRepository carRepo)
         {
             _signInManager = signInManager;
+            _carRepo = carRepo;
         }
         public void OnGet()
         {
+            Vehicles = _carRepo.GetAllVehiclesAccepted().OrderBy(v => v.Rate).Take(3).ToList();
         }
 
         public IActionResult OnGetLogout()
